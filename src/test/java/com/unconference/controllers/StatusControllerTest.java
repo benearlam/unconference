979421489665afbda2fa1345a4ea313746297b3f
Ben Earlam <bearlam@thoughtworks.com>
2015-06-21 20:47:44 +0100

    add test attributes

--- src/test/java/com/unconference/controllers/StatusControllerTest.java
+++ src/test/java/com/unconference/controllers/StatusControllerTest.java
@@ -1,6 +1,7 @@
 package com.unconference.controllers;
 
 import junit.framework.TestCase;
+import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
@@ -8,6 +9,7 @@ import static org.junit.Assert.assertThat;
 public class StatusControllerTest extends TestCase {
     StatusController controller = new StatusController();
 
+    @Test
     public void testStatus() throws Exception {
         assertThat(controller.status(), is("OK!"));
     }
