aed04dc6b82000c21c8d9a72eec2b7933172b034
Ben Earlam <bearlam@thoughtworks.com>
2015-06-22 22:20:02 +0100

    Put awssecret into place

--- deploy_to_eb.sh
+++ deploy_to_eb.sh
@@ -2,10 +2,13 @@
 
 SHA1=$1
 
+sed "s/<AWS_ACCESS_KEY_ID>/$AWS_ACCESS_KEY_ID/;s/<AWS_SECRET_ACCESS_KEY>/$AWS_SECRET_ACCESS_KEY/" awssecret.template > .awssecret
+
 # Create new Elastic Beanstalk version
 EB_BUCKET=elasticbeanstalk-eu-west-1-951298071900
 
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
+
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
 aws elasticbeanstalk create-application-version --application-name unconference \
