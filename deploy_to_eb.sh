ef8750ded87956c548078822fa9e9a89a39567bc
Ben Earlam <bearlam@thoughtworks.com>
2015-06-23 06:47:32 +0100

    manually install awscli with pip

--- deploy_to_eb.sh
+++ deploy_to_eb.sh
@@ -8,7 +8,9 @@ EB_BUCKET=elasticbeanstalk-eu-west-1-951298071900
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
+
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
+
 aws elasticbeanstalk create-application-version --application-name unconference \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
